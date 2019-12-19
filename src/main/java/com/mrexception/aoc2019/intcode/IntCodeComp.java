fa758233a25b98131070eb8f509c6bb6f29760c3
Robert McBride <rob@mrexception.com>
2019-12-19 00:52:47 -0800

    2019 day7 part2

--- src/main/java/com/mrexception/aoc2019/intcode/IntCodeComp.java
+++ src/main/java/com/mrexception/aoc2019/intcode/IntCodeComp.java
@@ -1,63 +1,79 @@
 package com.mrexception.aoc2019.intcode;
 
-import java.util.ArrayList;
-import java.util.List;
+import java.util.ArrayDeque;
+import java.util.Deque;
 
 public class IntCodeComp {
     private final int[] program;
     private int[] memory;
     private int ptr;
-    private final List<Integer> outputs = new ArrayList<>();
+    public final Deque<Integer> inputs = new ArrayDeque<>();
+    public final Deque<Integer> outputs = new ArrayDeque<>();
 
     public IntCodeComp(int[] program) {
         this.program = program;
     }
 
-    public void boot() {
+    public IntCodeComp boot() {
         ptr = 0;
         memory = new int[program.length];
         System.arraycopy(program, 0, memory, 0, program.length);
+        return this;
     }
 
     public int runWithInput(int noun, int verb) {
         program[1] = noun;
         program[2] = verb;
-        return run();
-    }
-
-    protected Instruction nextInstruction() {
-        Instruction instruction = new Instruction(memory, ptr);
-        ptr += instruction.getLength();
-        return instruction;
-    }
-
-    public int run(int... inputs) {
         boot();
+        main();
+        return defaultOutput();
+    }
 
-        int nextInput = 0;
-        Instruction next = nextInstruction();
-        while (next.getOpcode() != Opcode.HALT) {
-            Integer input = null;
-            if (next.getOpcode() == Opcode.INPUT) {
-                input = inputs[nextInput];
-                nextInput++;
-            }
-            int output = next.exec(memory, input);
-            if (next.getOpcode() == Opcode.OUTPUT) {
-                outputs.add(output);
-            } else if (next.getOpcode() == Opcode.JIT || next.getOpcode() == Opcode.JIF) {
-                if (output > -1) {
-                    ptr = output;
-                }
-            }
-            next = nextInstruction();
-        }
-
+    private int defaultOutput() {
         if (outputs.isEmpty()) {
             return memory[0];
         }
 
-        return outputs.get(outputs.size() - 1);
+        return outputs.removeLast();
+    }
+
+    protected Instruction nextInstruction() {
+        return new Instruction(memory, ptr);
+    }
+
+    public int run(int... in) {
+        boot();
+        for (int input : in) {
+            inputs.offer(input);
+        }
+        main();
+        return defaultOutput();
+    }
+
+    public boolean main() {
+        Instruction next = nextInstruction();
+        while (next.getOpcode() != Opcode.HALT) {
+            Integer input = null;
+            if (next.getOpcode() == Opcode.INPUT) {
+                input = inputs.poll();
+                if (input == null) {
+                    return false; // not complete - pause
+                }
+            }
+
+            int output = next.exec(memory, input);
+
+            if (next.getOpcode() == Opcode.OUTPUT) {
+                outputs.offer(output);
+            }
+            if (output > -1 && (next.getOpcode() == Opcode.JIT || next.getOpcode() == Opcode.JIF)) {
+                ptr = output;
+            } else {
+                ptr += next.getLength();
+            }
+            next = nextInstruction();
+        }
+        return true;
     }
 
     public int valueAt(int address) {
--- src/test/java/com/mrexception/aoc2019/Day7.java
+++ src/test/java/com/mrexception/aoc2019/Day7.java
@@ -34,8 +34,21 @@ public class Day7 {
         assertThat(partOne(program)).isEqualTo(398674);
     }
 
-    private int partOne(int[] testProg1) {
-        final IntCodeComp comp = new IntCodeComp(testProg1);
+    @Test
+    public void testPartTwo() throws Exception {
+        final int[] testProg1 = toInts(splitLine("3,26,1001,26,-4,26,3,27,1002,27,2,27,1,27,26,27,4,27,1001,28,-1,28,1005,28,6,99,0,0,5"));
+        final int[] testProg2 = toInts(splitLine("3,52,1001,52,-5,52,3,53,1,52,56,54,1007,54,5,55,1005,55,26,1001,54,-5,54,1105,1,12,1,53,54,53,1008,54,0,55,1001,55,1,55,2,53,55,53,4,53,1001,56,-1,56,1005,56,6,99,0,0,0,0,10"));
+
+        assertThat(partTwo(testProg1)).isEqualTo(139629729);
+        assertThat(partTwo(testProg2)).isEqualTo(18216);
+
+        final String progLine = processFile(inputFile)[0];
+        final int[] program = toInts(splitLine(progLine));
+        assertThat(partTwo(program)).isEqualTo(39431233);
+    }
+
+    private int partOne(int[] program) {
+        final IntCodeComp comp = new IntCodeComp(program);
         int a, b, c, d, e;
         int max = Integer.MIN_VALUE;
 
@@ -55,7 +68,46 @@ public class Day7 {
         return max;
     }
 
-    @Test
-    public void testPartTwo() throws Exception {
+    private int partTwo(int[] program) {
+        IntCodeComp a, b, c, d, e;
+        int max = Integer.MIN_VALUE;
+
+        List<List<Integer>> permutations = permutations(new Integer[]{5, 6, 7, 8, 9});
+        for (List<Integer> phases : permutations) {
+            a = new IntCodeComp(program).boot();
+            a.inputs.offer(phases.get(0));
+            a.inputs.offer(0);
+            b = new IntCodeComp(program).boot();
+            b.inputs.offer(phases.get(1));
+            c = new IntCodeComp(program).boot();
+            c.inputs.offer(phases.get(2));
+            d = new IntCodeComp(program).boot();
+            d.inputs.offer(phases.get(3));
+            e = new IntCodeComp(program).boot();
+            e.inputs.offer(phases.get(4));
+
+            int output;
+            while (true) {
+                a.main();
+                b.inputs.offer(a.outputs.poll());
+                b.main();
+                c.inputs.offer(b.outputs.poll());
+                c.main();
+                d.inputs.offer(c.outputs.poll());
+                d.main();
+                e.inputs.offer(d.outputs.poll());
+                boolean done = e.main();
+                output = e.outputs.poll();
+                if (done) {
+                    if (output > max) {
+                        max = output;
+                    }
+                    break;
+                } else {
+                    a.inputs.offer(output);
+                }
+            }
+        }
+        return max;
     }
 }
