95bcfb996d2f88810ba9c889b9cf9f894ff4d02f
Robert McBride <robert.mcbride@telus.com>
2024-01-11 06:37:59 -0800

    2023 day 12 - GOT IT - 5 second run time

--- src/main/java/com/mrexception/aoc2023/Day12.java
+++ src/main/java/com/mrexception/aoc2023/Day12.java
@@ -1,11 +1,6 @@
 package com.mrexception.aoc2023;
 
 import java.util.*;
-import java.util.Map.Entry;
-import java.util.stream.*;
-
-import java.util.function.*;
-import java.io.*;
 import java.nio.file.*;
 
 public class Day12 {
@@ -53,9 +48,9 @@ public class Day12 {
     log("Part Two Test: " + two);
     assert two == 525152;
 
-    // two = run(expand(readFile()));
-    // log("Part Two Real: " + two);
-    // assert two == 0;
+    two = run(expand(readFile()));
+    log("Part Two Real: " + two);
+    assert two == 204640299929836L;
   }
 
   private static long run(String[] input) {
@@ -68,13 +63,13 @@ public class Day12 {
       String pattern = line.split(" ")[0];
       Integer[] broken = toNums(line.split(" ")[1].split(","));
 
-      if (i % 100 == 0) {
-        log("Completed " + i);
-      }
+      // if (i % 100 == 0) {
+      // log("Completed " + i);
+      // }
 
-      // result += permute(pattern, broken);
-
-      result += recurse(0, broken, 0, pattern, cache);
+      // log(String.format("-------\nStarting: %s - %s", pattern,
+      // Arrays.toString(broken)));
+      result += recurse(pattern, broken, cache);
     }
 
     return result;
@@ -95,78 +90,53 @@ public class Day12 {
     return result;
   }
 
-  private static long permute(String original, Long[] broken) {
-    long result = 0;
-    Queue<String> q = new LinkedList<>();
-    q.add(original);
-
-    while (!q.isEmpty()) {
-      var pattern = q.poll();
-      var pos = pattern.indexOf('?');
-      if (pos > -1) {
-        String newPattern = pattern.substring(0, pos) + '.' + pattern.substring(pos + 1);
-        if (accept(newPattern, broken)) {
-          q.add(newPattern);
-        }
-        newPattern = pattern.substring(0, pos) + '#' + pattern.substring(pos + 1);
-        if (accept(newPattern, broken)) {
-          q.add(newPattern);
-        }
-      } else {
-        result++;
-      }
-    }
-    return result;
-
-  }
-
-  private static long recurse(int groupIdx, Integer[] group, int pos, String pattern,
-      Map<String, Long> memo) {
+  private static long recurse(String pattern, Integer[] group, Map<String, Long> memo) {
     String key = String.format("%s-%s", Arrays.toString(group), pattern);
     if (memo.containsKey(key)) {
-      return memo.get(key);
+      long result = memo.get(key);
+      // log(String.format("Return from cache - %s - %s - %s", pattern,
+      // Arrays.toString(group), result));
+      return result;
     }
 
-    if (pos >= pattern.length()) {
+    if (pattern.isEmpty()) {
 
       // if we hit the end of the string, and we haven't finished all groups
-      if (groupIdx < group.length) {
-        // log(String.format("A - %s is invalid at %s - %s", pattern, pos,
+      if (group.length > 0) {
+        // log(String.format("A - %s is invalid - %s", pattern,
         // Arrays.toString(group)));
         memo.put(key, 0L);
         return 0;
       }
 
       // // off the end of the pattern
-      // log(String.format("A - %s is valid %s - %s", pattern, Arrays.toString(group),
-      // groupIdx));
+      // log(String.format("A - %s is valid %s", pattern, Arrays.toString(group)));
       memo.put(key, 1L);
       return 1;
     }
 
-    if (groupIdx >= group.length) {
+    if (group.length == 0) {
       // all groups done - make sure there are no more '#'
-      for (int i = pos; i < pattern.length(); i++) {
+      for (int i = 0; i < pattern.length(); i++) {
         if (pattern.charAt(i) == '#') {
-          // log(String.format("B - %s is invalid at %s - %s", pattern, pos,
+          // log(String.format("B - %s is invalid - %s", pattern,
           // Arrays.toString(group)));
           memo.put(key, 0L);
           return 0;
         }
       }
 
-      // log(String.format("B - %s is valid %s - %s", pattern, Arrays.toString(group),
-      // groupIdx));
+      // log(String.format("B - %s is valid %s", pattern, Arrays.toString(group)));
       memo.put(key, 1L);
       return 1;
     }
 
-    if (pattern.charAt(pos) == '#') {
-      var len = group[groupIdx];
+    if (pattern.charAt(0) == '#') {
+      var len = group[0];
 
       // there must be at least enough chars to fill the group
-      if (pos + len > pattern.length()) {
-        // log(String.format("C - %s is invalid at %s - %s", pattern, pos,
+      if (len > pattern.length()) {
+        // log(String.format("C - %s is invalid - %s", pattern,
         // Arrays.toString(group)));
         memo.put(key, 0L);
         return 0;
@@ -174,9 +144,9 @@ public class Day12 {
 
       // the next group of chars must not contain a '.'
       // it can contain a '?' since we can substitute that for a '#'
-      for (int i = pos; i < pos + len; i++) {
+      for (int i = 0; i < len; i++) {
         if (pattern.charAt(i) == '.') {
-          // log(String.format("D - %s is invalid at %s - %s", pattern, pos,
+          // log(String.format("D - %s is invalid - %s", pattern,
           // Arrays.toString(group)));
           memo.put(key, 0L);
           return 0;
@@ -184,98 +154,49 @@ public class Day12 {
       }
 
       // the immediate next char must not be a '#'
-      if (pos + len < pattern.length() && pattern.charAt(pos + len) == '#') {
-        // log(String.format("E - %s is invalid at %s - %s - %s", pattern, pos,
-        // Arrays.toString(group), groupIdx));
+      if (len < pattern.length() && pattern.charAt(len) == '#') {
+        // log(String.format("E - %s is invalid - %s", pattern,
+        // Arrays.toString(group)));
         memo.put(key, 0L);
         return 0;
       }
 
-      pos = pos + len;
-      groupIdx++;
-
       // special case, if the next char is a '?' it MUST be a '.'
+      var nextGroup = Arrays.copyOfRange(group, 1, group.length);
+      String nextPattern;
       long result = 0;
-      if (pos < pattern.length() && pattern.charAt(pos) == '?') {
-        String newPattern = pattern.substring(0, pos) + '.' + pattern.substring(pos + 1);
-        result = recurse(groupIdx, group, pos, newPattern, memo);
+      if (len < pattern.length() && pattern.charAt(len) == '?') {
+        nextPattern = '.' + pattern.substring(len + 1);
       } else {
-        result = recurse(groupIdx, group, pos, pattern, memo);
+        nextPattern = pattern.substring(len);
       }
+      result = recurse(nextPattern, nextGroup, memo);
 
       memo.put(key, result);
       return result;
     }
 
-    if (pattern.charAt(pos) == '.') {
-      long result = recurse(groupIdx, group, pos + 1, pattern, memo);
+    if (pattern.charAt(0) == '.') {
+      long result = recurse(pattern.substring(1), group, memo);
       memo.put(key, result);
       return result;
     }
 
     long result = 0;
-    if (pattern.charAt(pos) == '?') {
+    if (pattern.charAt(0) == '?') {
       // recurse twice for two new patterns
-      String newPattern = pattern.substring(0, pos) + '.' + pattern.substring(pos + 1);
-      // log(pattern + " -> " + newPattern);
-      result += recurse(groupIdx, group, pos, newPattern, memo);
-      // TODO: problem - if the PREVIOUS char was a '#', this one can't be! or we
-      // violate the previous group
-      newPattern = pattern.substring(0, pos) + '#' + pattern.substring(pos + 1);
-      result += recurse(groupIdx, group, pos, newPattern, memo);
-    } else {
-      result = recurse(groupIdx, group, pos + 1, pattern, memo);
+      String nextPattern = '.' + pattern.substring(1);
+      // log(pattern + " -> " + nextPattern);
+      result += recurse(nextPattern, group, memo);
+      nextPattern = '#' + pattern.substring(1);
+      result += recurse(nextPattern, group, memo);
+
+      memo.put(key, result);
+      return result;
     }
 
-    memo.put(key, result);
-    return result;
-  }
-
-  private static boolean accept(String perm, Long[] broken) {
-    char[] chars = perm.toCharArray();
-
-    boolean complete = !perm.contains("?");
-
-    int q = 0;
-    int l = 0;
-    for (int i = 0; i < chars.length; i++) {
-      if (chars[i] == '?') {
-        break;
-      }
-
-      if (chars[i] == '#') {
-        l++;
-      }
-
-      if (chars[i] == '.' && l > 0) {
-        if (q >= broken.length) {
-          return false;
-        }
-        var n = broken[q++];
-        if (n != l) {
-          return false;
-        }
-        l = 0;
-      }
-    }
-
-    if (l > 0) {
-      if (q >= broken.length) {
-        return false;
-      }
-      var n = broken[q++];
-      if (!complete && n < l) {
-        return false;
-      }
-      if (complete && n != l) {
-        return false;
-      }
-    }
-
-    if (complete) {
-      return q >= broken.length;
-    }
-
-    return true;
+    // should never get here
+    log("HOW DID WE GET HERE?");
+    return 0;
   }
 }
